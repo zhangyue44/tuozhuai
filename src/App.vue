351a7b1123d75cb88a643afa923cf6d4e1c22695
zhangyue44 <zy1617768774@163.com>
2022-04-01 02:48:43 +0800

    feat: 添加手动撸拖拽布局代码

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,24 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+}
+</script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
