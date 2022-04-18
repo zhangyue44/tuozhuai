1c96a13d423284a9c7441ea862f1f83e3d5e97dc
zhangyue44 <zy1617768774@163.com>
2022-04-19 01:15:48 +0800

    Revert "fix: 测试"
    This reverts commit 032f5b28ee437eaecc566fc1642ae89ac024f24d.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import VueDragResize from "vue-drag-resize";
-import VueDraggableResizable from "vue-draggable-resizable";
-import "vue-draggable-resizable/dist/VueDraggableResizable.css";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import VueDragResize from 'vue-drag-resize'
+import VueDraggableResizable from 'vue-draggable-resizable'
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-Vue.component("vue-drag-resize", VueDragResize);
-Vue.component("vue-draggable-resizable", VueDraggableResizable);
+Vue.component('vue-drag-resize', VueDragResize)
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App)
+}).$mount('#app')
